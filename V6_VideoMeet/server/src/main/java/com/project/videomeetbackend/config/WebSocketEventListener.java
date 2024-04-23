b4d0597ee3799464de68c546e99aceb15478e245
Deep_Lajpal <lajpaldeep@gmail.com>
2024-04-23 16:08:54 +0530

    Register and Login Functionality is Working fine

--- V6_VideoMeet/server/src/main/java/com/project/videomeetbackend/config/WebSocketEventListener.java
+++ V6_VideoMeet/server/src/main/java/com/project/videomeetbackend/config/WebSocketEventListener.java
@@ -12,8 +12,6 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
-import com.project.videomeetbackend.chat.ChatMessage;
-
 @Component
 @Slf4j
 @RequiredArgsConstructor
